8e286ea0bbaeb1ff39eeb2331184565dd9493c1a
Victor Rodrigues <91481122+DevzVictor@users.noreply.github.com>
2022-11-27 11:32:03 -0300

    feat(product): implement dto validation

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,6 +1,34 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString, IsUrl } from 'class-validator';
+
 export class CreateProductDto {
+  @IsString()
+  @ApiProperty({
+    description: 'Nome do produto',
+    example: 'Pizza Portuguesa',
+  })
   name: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'Descrição do produto',
+    example: 'Deliciosa pizza portuguesa com calabresa e cebola',
+  })
   description: string;
+
+  @IsNumber({
+    maxDecimalPlaces: 2,
+  })
+  @ApiProperty({
+    description: 'Preço do produto',
+    example: '20.00',
+  })
   price: number;
+
+  @IsUrl()
+  @ApiProperty({
+    description: 'Imagem do produto',
+    example: 'https://i.imgur.com/hNE75Iw.png',
+  })
   image: string;
 }
