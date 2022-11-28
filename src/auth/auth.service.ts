5f81c2c52922e59bfaa3657bba982bb221577059
Victor Rodrigues <91481122+DevzVictor@users.noreply.github.com>
2022-11-27 23:37:16 -0300

    feat(auth): implement login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { LoginResponseDto } from './dto/login-response.dto';
@@ -6,7 +7,10 @@ import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async login(loginDto: LoginDto): Promise<LoginResponseDto> {
     const { nickname, password } = loginDto;
@@ -27,6 +31,9 @@ export class AuthService {
 
     delete user.password;
 
-    return;
+    return {
+      token: this.jwtService.sign({ nickname }),
+      user,
+    };
   }
 }
