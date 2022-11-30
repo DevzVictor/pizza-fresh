011aa97a6e85d2acca90beb3436a360842f27bae
Victor Rodrigues <91481122+DevzVictor@users.noreply.github.com>
2022-11-29 22:02:06 -0300

    bug loko

--- src/table/table.controller.ts
+++ src/table/table.controller.ts
@@ -13,8 +13,8 @@ import {
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
-import { CreateTableDto } from './dto/create-table.dto';
-import { UpdateTableDto } from './dto/update-table.dto';
+import type { CreateTableDto } from './dto/create-table.dto';
+import type { UpdateTableDto } from './dto/update-table.dto';
 import { Table } from './entities/table.entity';
 import { TableService } from './table.service';
 
